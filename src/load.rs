f311667d330bc0f654827177b4f0d30e7ee2d719
David Jonas <djonas@noip.com>
2020-04-01 00:10:22 -0700

    Revamp error handling with anyhow, add hg support

--- src/load.rs
+++ src/load.rs
@@ -1,49 +1,124 @@
 use std::ffi::OsString;
+use std::fmt;
 use std::io;
 use std::path::Path;
 use std::process::Command;
 use std::str::from_utf8;
+use std::str::FromStr;
+
+use anyhow::{Context, Error, Result};
 
 use cargo_lock::Lockfile;
 
-use crate::{verbose, Error, ErrorMsg};
+const ALL_SOURCES: [Source; 3] = [Source::File, Source::Git, Source::Mercurial];
 
-pub fn load(source: &str, base: &str) -> Result<Lockfile, Error> {
+#[derive(Clone, Debug)]
+pub enum Source {
+    File,
+    Git,
+    Mercurial,
+}
+
+impl Source {
+    pub fn all() -> &'static [Source] {
+        &ALL_SOURCES
+    }
+
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::File => "file",
+            Self::Git => "git",
+            Self::Mercurial => "hg",
+        }
+    }
+}
+
+impl fmt::Display for Source {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(self.as_str())
+    }
+}
+
+impl FromStr for Source {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "file" => Source::File,
+            "git" => Source::Git,
+            "hg" => Source::Mercurial,
+            _ => return Err(()),
+        })
+    }
+}
+
+#[derive(Debug)]
+struct LoadError {
+    errors: Vec<Error>,
+}
+
+impl std::error::Error for LoadError {}
+
+impl fmt::Display for LoadError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for e in &self.errors {
+            writeln!(f, "")?;
+            write!(f, "  {:#}", e)?;
+        }
+
+        Ok(())
+    }
+}
+
+pub fn load(sources: &[Source], fileish: &str, base: &str) -> Result<Lockfile> {
     let base_path = if base.is_empty() {
         Path::new(".")
     } else {
         Path::new(base.trim_end_matches(std::path::MAIN_SEPARATOR))
     };
 
-    let path = if source.is_empty() {
-        base_path.join("Cargo.lock")
-    } else {
-        base_path.join(source)
-    };
+    let mut err = LoadError { errors: vec![] };
 
-    if path.exists() {
-        return Ok(Lockfile::load(path)?);
+    for source in sources {
+        match source.load(fileish, base_path).context(source.as_str()) {
+            Ok(f) => return Ok(f),
+            Err(e) => err.errors.push(e),
+        }
     }
 
-    match load_git(source, base_path) {
-        Ok(f) => Ok(f),
-        Err(e) => {
-            //
-            // Try the next one. load_hg()?
-            //
-            Err(Error::with_err(ErrorMsg::NotFound, e))
+    Err(err.into())
+}
+
+impl Source {
+    fn load(&self, fileish: &str, base_path: &Path) -> Result<Lockfile> {
+        match self {
+            Self::File => load_file(fileish, base_path),
+            Self::Git => load_git(fileish, base_path),
+            Self::Mercurial => load_hg(fileish, base_path),
         }
     }
 }
 
-fn load_git(gitref: &str, base_path: &Path) -> Result<Lockfile, Error> {
+fn load_file(fileish: &str, base_path: &Path) -> Result<Lockfile> {
+    let path = if fileish.is_empty() {
+        base_path.join("Cargo.lock")
+    } else {
+        base_path.join(fileish)
+    };
+
+    if path.exists() {
+        Ok(Lockfile::load(path)?)
+    } else {
+        Err(io::Error::new(io::ErrorKind::NotFound, path.display().to_string()).into())
+    }
+}
+
+fn load_git(gitref: &str, base_path: &Path) -> Result<Lockfile> {
     let gitpath: OsString = if gitref.contains(':') {
         gitref.into()
     } else {
-        let path = base_path.join("Cargo.lock");
         let mut gitpath = OsString::from(gitref);
         gitpath.push(":");
-        gitpath.push(path);
+        gitpath.push(base_path.join("Cargo.lock"));
 
         gitpath
     };
@@ -51,19 +126,40 @@ fn load_git(gitref: &str, base_path: &Path) -> Result<Lockfile, Error> {
     let output = Command::new("git").arg("show").arg(&gitpath).output()?;
 
     if !output.status.success() {
-        let code = output.status.code().unwrap_or(1);
-        verbose(|| {
-            let stderr: String = from_utf8(&output.stderr)
-                .map(Into::into)
-                .unwrap_or_else(|e| format!("{}", e));
-            format!(
-                "failed to load from git path {}; {}",
-                gitpath.into_string().unwrap_or_else(|_| "bad path".into()),
-                stderr.trim_end()
-            )
-        });
-        let e = io::Error::from_raw_os_error(code);
-        return Err(Error::with_err(ErrorMsg::CommandFailed("git"), e));
+        let stderr: String = from_utf8(&output.stderr).map(Into::into)?;
+        return Err(Error::msg(stderr.trim_end().to_string()));
+    }
+
+    Ok(from_utf8(&output.stdout)?.parse()?)
+}
+
+fn load_hg(fileish: &str, base_path: &Path) -> Result<Lockfile> {
+    let mut file: OsString = base_path.join("Cargo.lock").into();
+
+    // If they don't supply anything or they use the git default, convert to hg
+    let hgref: OsString = if fileish.is_empty() || fileish == "HEAD" {
+        ".".into()
+    // If they use the colon separated git format take their word for the file path.
+    } else if fileish.contains(':') {
+        let mut split = fileish.splitn(2, ':');
+        let hgref = split.next().expect("split 1");
+        file = split.next().expect("split 2").into();
+        hgref.into()
+    // And sometimes a ref is just a ref
+    } else {
+        fileish.into()
+    };
+
+    let output = Command::new("hg")
+        .arg("cat")
+        .arg("-r")
+        .arg(&hgref)
+        .arg(&file)
+        .output()?;
+
+    if !output.status.success() {
+        let stderr: String = from_utf8(&output.stderr).map(Into::into)?;
+        return Err(Error::msg(stderr.trim_end().to_string()));
     }
 
     Ok(from_utf8(&output.stdout)?.parse()?)
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-use std::fmt;
-use std::sync::atomic::{AtomicBool, Ordering::Relaxed};
-
+use anyhow::{anyhow, Result};
 use argh::FromArgs;
-use prettytable::{cell, format::TableFormat, row, Table};
+use prettytable::{cell, row, Table};
 
 mod diff;
 mod load;
 
 use diff::*;
 use load::*;
-
-static VERBOSE: AtomicBool = AtomicBool::new(false);
 
 #[derive(Debug, FromArgs)]
 /// Diff your lock file, see what's changed.
@@ -19,136 +15,38 @@ struct Opts {
     #[argh(option, short = 'p', default = r#""".into()"#)]
     path: String,
 
-    /// the file, git ref, or git ref with filename to compare from
+    /// the file, vcs ref, or vcs ref with filename to compare from. To force the use of a
+    /// particular vcs, prepend it with a colon. E.g. "hg:.". The ref is required if the vcs is
+    /// specified.
     #[argh(option, default = r#""HEAD".into()"#)]
     from: String,
 
-    /// the file, git ref, or git ref with filename to compare to
+    /// the file, vcs ref, or vcs ref with filename to compare to. To force the use of a
+    /// particular vcs, prepend it with a colon. E.g. "hg:.". The ref is required if the vcs is
+    /// specified.
     #[argh(option, default = r#""".into()"#)]
     to: String,
 
     /// include links to where possible
     #[argh(switch, short = 'l')]
     links: bool,
-
-    /// show some extra messages
-    #[argh(switch, short = 'v')]
-    verbose: bool,
 }
 
-#[derive(Debug, PartialEq)]
-enum ErrorMsg {
-    None,
-    NotFound,
-    CommandFailed(&'static str),
-}
-
-impl fmt::Display for ErrorMsg {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        use ErrorMsg::*;
-        match self {
-            None => return Ok(()),
-            NotFound => f.write_str("not found"),
-            CommandFailed(cmd) => write!(f, "'{}' command failed", cmd),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct Error {
-    msg: ErrorMsg,
-    source: Option<Box<dyn std::error::Error + 'static>>,
-}
-
-impl Error {
-    fn with_err<E: std::error::Error + 'static>(msg: ErrorMsg, err: E) -> Self {
-        Error {
-            msg,
-            source: Some(Box::new(err)),
-        }
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        if self.msg != ErrorMsg::None {
-            self.msg.fmt(f)?;
-        }
-
-        if let Some(err) = &self.source {
-            if self.msg != ErrorMsg::None {
-                f.write_str("\nErrors:\n  + ")?;
-            }
-            write!(f, "{:?}", err)?;
-        }
-
-        Ok(())
-    }
-}
-
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        self.source.as_ref().map(|v| v.as_ref())
-    }
-}
-
-impl From<ErrorMsg> for Error {
-    fn from(msg: ErrorMsg) -> Self {
-        Self { msg, source: None }
-    }
-}
-
-impl From<cargo_lock::error::Error> for Error {
-    fn from(e: cargo_lock::error::Error) -> Self {
-        Self {
-            msg: ErrorMsg::None,
-            source: Some(Box::new(e)),
-        }
-    }
-}
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Self {
-        Self {
-            msg: ErrorMsg::None,
-            source: Some(Box::new(e)),
-        }
-    }
-}
-
-impl From<std::str::Utf8Error> for Error {
-    fn from(e: std::str::Utf8Error) -> Self {
-        Self {
-            msg: ErrorMsg::None,
-            source: Some(Box::new(e)),
-        }
-    }
-}
-
-fn verbose<F: FnOnce() -> String>(msg_fn: F) {
-    if VERBOSE.load(Relaxed) {
-        eprintln!("{}", msg_fn());
-    }
-}
-
-fn main() -> Result<(), i32> {
+fn main() -> Result<()> {
     let opts: Opts = match std::env::args().nth(1) {
         Some(arg) if arg == "lockdiff" => argh::cargo_from_env(),
         _ => argh::from_env(),
     };
 
-    if opts.verbose {
-        VERBOSE.store(true, Relaxed);
-    }
+    let (from_sources, from_fileish) = parse_source_opt(&opts.from);
 
-    let from = load(&opts.from, &opts.path).map_err(|e| {
-        eprintln!("could not read 'from'; {}", e);
-        1
-    })?;
+    let from = load(&from_sources, from_fileish, &opts.path)
+        .map_err(|e| anyhow!("could not read 'from': {}", e))?;
 
-    let to = load(&opts.to, &opts.path).map_err(|e| {
-        eprintln!("could not read 'to'; {}", e);
-        1
-    })?;
+    let (to_sources, to_fileish) = parse_source_opt(&opts.to);
+
+    let to = load(&to_sources, to_fileish, &opts.path)
+        .map_err(|e| anyhow!("could not read 'to': {}", e))?;
 
     let diff = diff(&from, &to);
 
@@ -161,19 +59,34 @@ fn main() -> Result<(), i32> {
     Ok(())
 }
 
-fn format_markdown() -> TableFormat {
+fn parse_source_opt(opt: &str) -> (Vec<Source>, &str) {
+    let mut iter = opt.splitn(2, ':');
+    let head = iter.next();
+    let rest = iter.next();
+
+    if opt.is_empty() || rest.is_none() {
+        return (Source::all().to_vec(), opt);
+    }
+
+    let source = match head.expect("head").parse::<Source>() {
+        Ok(s) => s,
+        Err(_) => {
+            return (Source::all().to_vec(), opt);
+        }
+    };
+
+    (vec![source], rest.unwrap_or_default())
+}
+
+fn print_markdown(diff: &Diff, links: bool) {
     use prettytable::format::{FormatBuilder, LinePosition::*, LineSeparator};
 
-    FormatBuilder::new()
+    let format = FormatBuilder::new()
         .borders('|')
         .column_separator('|')
         .separator(Title, LineSeparator::new('-', '|', '|', '|'))
         .padding(1, 1)
-        .build()
-}
-
-fn print_markdown(diff: &Diff, links: bool) {
-    let format = format_markdown();
+        .build();
 
     let mut table = Table::new();
 
